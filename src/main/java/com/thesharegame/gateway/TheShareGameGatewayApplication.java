3c945670ae80f3a2f5f0d91a3161ea4db9763827
Kevin Dunne <kevinmdunne@users.noreply.github.com>
2024-09-05 22:19:15 +0100

    first commit

---
+++ src/main/java/com/thesharegame/gateway/TheShareGameGatewayApplication.java
@@ -0,0 +1,27 @@
+package com.thesharegame.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+public class TheShareGameGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheShareGameGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/portfolio/**")
+						.filters(f -> f.addRequestHeader("Hello", "World"))
+						.uri("http://localhost:8083/portfolio/"))
+				.build();
+	}
+
+}
---
+++ src/test/java/com/thesharegame/gateway/TheShareGameGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thesharegame.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheShareGameGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
