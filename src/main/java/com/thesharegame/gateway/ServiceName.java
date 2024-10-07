9492cc17b0e7d0170f20b1619a96b0f9ea59e73c
Kevin Dunne <kevinmdunne@users.noreply.github.com>
2024-10-07 23:24:30 +0100

    enable eureka

---
+++ src/main/java/com/thesharegame/gateway/ServiceName.java
@@ -0,0 +1,13 @@
+package com.thesharegame.gateway;
+
+public enum ServiceName {
+    USER_SERVICE("thesharegame-user-service"),
+    PORTFOLIO_SERVICE("thesharegame-portfolio-service"),
+    SHARE_SERVICE("share-service");
+
+    public final String serviceName;
+
+    private ServiceName(String serviceName){
+        this.serviceName = serviceName;
+    }
+}
--- src/main/java/com/thesharegame/gateway/TheShareGameGatewayApplication.java
+++ src/main/java/com/thesharegame/gateway/TheShareGameGatewayApplication.java
@@ -1,5 +1,10 @@
 package com.thesharegame.gateway;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -9,6 +14,12 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TheShareGameGatewayApplication {
+
+	@Value("${gateway.host}")
+	private String host;
+
+	@Autowired
+	private EurekaClient eurekaClient;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TheShareGameGatewayApplication.class, args);
@@ -20,16 +31,22 @@ public class TheShareGameGatewayApplication {
 				.route(p -> p
 						.path("/portfolio/**")
 						.filters(f -> f.addRequestHeader("Hello", "World"))
-						.uri("http://localhost:8083/portfolio/"))
+						.uri(getBaseUri(ServiceName.PORTFOLIO_SERVICE) + "portfolio/"))
 				.route(p -> p
 						.path("/user/**")
 						.filters(f -> f.addRequestHeader("Hello", "World"))
-						.uri("http://localhost:8081/user/"))
+						.uri(getBaseUri(ServiceName.USER_SERVICE) + "user/"))
 				.route(p -> p
 						.path("/share/**")
 						.filters(f -> f.addRequestHeader("Hello", "World"))
-						.uri("http://localhost:8082/share/"))
+						.uri("http://" + host + ":8082/share/"))
 				.build();
+	}
+
+	private String getBaseUri(ServiceName serviceName){
+		Application application = eurekaClient.getApplication(serviceName.serviceName);
+		InstanceInfo service = application.getInstances().get(0);
+		return service.getHomePageUrl();
 	}
 
 }
